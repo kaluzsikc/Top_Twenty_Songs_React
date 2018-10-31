993128eec2010d0212c483817272b62bbc16fc4f
kaluzsikc <kaluzsikc@gmail.com>
2018-10-31 00:23:44 +0000

    Songs are being rendered

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
 
-const SongDetail = ( ) => {
-    return (
-            
-            <p>Song</p>
+const SongDetail = (props ) => {
+    const rank = props.song.category.attributes["im:id"];
+    const artistName = props.song['im:artist'].label;
+    const songTitle = props.song['im:name'].label;
+    const img = props.song["im:image"][0].label;
 
+    if (!props.song)
+    return null;
+
+    return (
+         <div>
+            <p>Position:{rank}</p>
+            <p>{songTitle}</p> 
+            <h4>{artistName}</h4>
+            <img src={img}/>
+         </div>   
     )
 };
 
